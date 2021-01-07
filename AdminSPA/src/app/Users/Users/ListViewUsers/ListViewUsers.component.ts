deef985e8be4a805084fa404b08931cdfbbbe56f
Acengen <kocic999@yahoo.com>
2021-01-07 15:52:29 +0100

    Changes

--- AdminSPA/src/app/Users/Users/ListViewUsers/ListViewUsers.component.ts
+++ AdminSPA/src/app/Users/Users/ListViewUsers/ListViewUsers.component.ts
@@ -1,6 +1,10 @@
 import { UserServiceService } from './../../../Service/UserService.service';
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/Interfaces/User';
+import { Store } from '@ngrx/store';
+
+import * as fromReducer from '../../store/userReducer.reducer'
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: '[app-ListViewUsers]',
@@ -10,14 +14,14 @@ import { User } from 'src/app/Interfaces/User';
 export class ListViewUsersComponent implements OnInit {
   @Input() user:User;
   @Input() index:number;
-  constructor(private service:UserServiceService) { }
+  constructor(private service:UserServiceService,private store:Store<fromReducer.AppState>) { }
 
   ngOnInit() {
-   
+       
   }
 
   deleteUser(id,index) {
-      this.service.deleteUser(id,index);
+      this.service.deleteUser(id,index).subscribe();
   }
 
 }
